6b6186d05023458ad348190f1c008ec2f4e4d8d4
jaiswalA06 <arushijais002@gmail.com>
2026-07-24 12:29:27 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+
+        int length=nums.size();
+        vector<int> ans(2 * length);
+        for (int i=0;i<length;i++){
+            ans[i]=nums[i];
+            ans[i+length]=nums[i];
+        }
+
+        return ans;
+    }
+};
