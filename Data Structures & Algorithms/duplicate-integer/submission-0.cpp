aca79558d54bbb0a7a34ca6e66d90f3e023e1967
jaiswalA06 <arushijais002@gmail.com>
2026-07-28 19:01:11 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,15 @@
+#include<unordered_set>
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+
+        for(int i:nums){
+            if(s.count(i))
+                return true;
+
+            s.insert(i);
+        }
+        return false;
+    }
+};
