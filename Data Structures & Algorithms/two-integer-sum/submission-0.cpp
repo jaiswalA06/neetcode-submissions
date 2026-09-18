c1eb09aac7b9d4335e1fc7caaeeda2be0db5b661
Arushi Jaiswal <arushijais002@gmail.com>
2026-09-18 23:38:38 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,21 @@
+//brute force
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result;
+                }
+
+            }
+        }
+        return result;
+    }
+};
+
+// a better approach would be 2 pointer
+// and best approach = hashing
