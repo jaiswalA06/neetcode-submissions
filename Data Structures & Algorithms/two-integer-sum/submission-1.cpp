dd7521849e210121fa48121053039fcffdabc23c
Arushi Jaiswal <arushijais002@gmail.com>
2026-09-19 23:06:36 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int tar) {
+        unordered_map<int, int> m;
+        vector<int> ans;
+
+        for (int i = 0; i < arr.size(); i++) {
+            int first = arr[i];
+            int sec = tar - first;
+
+            if (m.find(sec) != m.end()) {
+                ans.push_back(i);
+                ans.push_back(m[sec]);
+                break;
+            }
+
+            m[first] = i;
+        }
+
+        sort(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
